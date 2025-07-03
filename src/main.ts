01d543063ae05c84874d2aabc5be7b3a749eeacc
Michiel de Jong <michiel@pondersource.com>
2025-07-03 16:13:42 +0200

    createSqlTable('./google-calendar.yaml', '/users/me/calendarList', 'items');

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { readFile } from 'fs';
 import { parse } from 'yaml'
-function fetchGoogleCalendar(): void {
-  readFile('./google-calendar.yaml', function(err, data) {
+// import * as sqlite from 'sqlite3';
+
+
+function createSqlTable(specFile: string, endPoint: string, rowsFrom: string): void {
+  readFile(specFile, function(err, data) {
     if (err) {
       throw err;
     }
@@ -16,10 +19,50 @@ function fetchGoogleCalendar(): void {
       console.error('No "paths" property found in YAML.');
       return;
     }
-    console.log(openApiSpec.paths['/users/me/calendarList']?.get?.responses?.['200']?.content?.['application/json']?.schema);
-    console.log(Object.keys(openApiSpec.components?.schemas?.['CalendarList']?.properties));
-    console.log(Object.keys(openApiSpec.components?.schemas?.['CalendarListEntry']?.properties));
+    let schema = openApiSpec.paths[endPoint]?.get?.responses?.['200']?.content?.['application/json']?.schema;
+    if (typeof schema['$ref'] === 'string') {
+      const pathParts = schema['$ref'].substring(2).split('/');
+      if (pathParts.length < 2 || pathParts[0] !== 'components' || pathParts[1] !== 'schemas') {
+        console.error('Invalid schema reference:', schema['$ref']);
+        return;
+      }
+      schema = openApiSpec.components?.schemas?.[schema['$ref'].split('/').pop()];
+    }
+    // console.log(schema);
+    let whatWeWant = schema?.properties?.[rowsFrom].items;
+    // console.log(whatWeWant);
+    if (typeof whatWeWant['$ref'] === 'string') {
+      const pathParts = whatWeWant['$ref'].substring(2).split('/');
+      if (pathParts.length < 2 || pathParts[0] !== 'components' || pathParts[1] !== 'schemas') {
+        console.error('Invalid schema reference:', whatWeWant['$ref']);
+        return;
+      }
+      const schemaName = pathParts[2];
+      whatWeWant = openApiSpec.components?.schemas?.[schemaName]?.properties;
+    }
+    // console.log(whatWeWant)
+    const rowSpecs = (whatWeWant ? Object.entries(whatWeWant).map(([key, value]) => {
+      const type = (value as { type: string }).type;
+      if (type === 'string') {
+        return `${key} TEXT,`;
+      } else if (type === 'integer') {
+        return `${key} INTEGER,`;
+      } else if (type === 'boolean') {
+        return `${key} BOOLEAN,`;
+      } else return '';
+    }) : []);
+    const createTableQuery = `
+CREATE TABLE data(
+  ${rowSpecs.join('\n  ')}
+) STRICT
+    `;
+    console.log(createTableQuery);
+    // Execute SQL statements from strings.
+    // const database = new sqlite.Database(':memory:');
+    // database.serialize(() => {
+    //   database.run(createTableQuery);
+    // });
   });
 }
 // ...
-fetchGoogleCalendar();
+createSqlTable('./google-calendar.yaml', '/users/me/calendarList', 'items');
