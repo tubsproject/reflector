4ca62f5fde80b971229ac550c5305bc01fd00673
Michiel de Jong <michiel@pondersource.com>
2025-07-03 15:31:55 +0200

    looks like my google json file has some funny linebreaks or something

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
-import { readFile } from 'fs/promises';
+import { readFile } from 'fs';
 
-
-async function fetchGoogleCalendar(): Promise<void> {
-  const openApiJsonStr = await readFile('./google-calendar.json');
-  console.log(openApiJsonStr.toString());
-  const openApiSpec = JSON.parse(openApiJsonStr.toString());
-  console.log(Object.keys(openApiSpec.paths));
+function fetchGoogleCalendar(): void {
+  readFile('./slack_web_openapi_v2.json', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    let openApiSpec;
+    try {
+      openApiSpec = JSON.parse(data.toString());
+    } catch (parseErr) {
+      console.error('Failed to parse JSON:', parseErr.message);
+      return;
+    }
+    if (!openApiSpec.paths) {
+      console.error('No "paths" property found in JSON.');
+      return;
+    }
+    console.log(Object.keys(openApiSpec.paths));
+  });
 }
 // ...
 fetchGoogleCalendar();
